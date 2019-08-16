454066a2bd1b9a4e784fd50fc016b35a1d70913a
liangshan <loveliangshan@126.com>
2019-08-16 19:37:01 +0800

    no message

--- test/job/service.test.js
+++ test/job/service.test.js
@@ -22,9 +22,11 @@ const baseData = {
   appid: 'A23'
 }
 
-const recommendJobKeys = ["aaa", "applyType", "bestEmployerLabel", "bestEmployerType", "businessIconPath", "chatWindow", "cityDistrict", "cityId", "commercializationType", "companyAutoID", "companyLogo", "companyName", "companyNumber", "companySize", "companyUrl", "deliveryPath", "distance", "distanceSubways", "education", "emplType", "expandCount", "fastPosition", "featureLabel", "feedbackRation", "futureJob", "futureJobUrl", "hasAppliedPosition", "industry", "industryName", "jobDesc", "jobId", "jobType", "menVipLevel", "menVipUrl", "name", "needMajor", "number", "positionSourceType", "positionURL", "property", "publishTime", "publishTimeDt", "qualifications", "refreshLevelType", "roles", "rootOrgId", "salary", "salary60", "salaryReal", "score", "scoreState", "skillLabel", "staff", "staffId", "tags", "tradingArea", "welfareLabel", "workCity", "workingExp", "workingExpCode"]
-
-describe('职位领域: requestRecommendJobs', function () {
+/**
+ * 推荐职位
+ */
+const recommendJobKeys = ["applyType", "bestEmployerLabel", "bestEmployerType", "businessIconPath", "chatWindow", "cityDistrict", "cityId", "commercializationType", "companyAutoID", "companyLogo", "companyName", "companyNumber", "companySize", "companyUrl", "deliveryPath", "distance", "distanceSubways", "education", "emplType", "expandCount", "fastPosition", "featureLabel", "feedbackRation", "futureJob", "futureJobUrl", "hasAppliedPosition", "industry", "industryName", "jobDesc", "jobId", "jobType", "menVipLevel", "menVipUrl", "name", "needMajor", "number", "positionSourceType", "positionURL", "property", "publishTime", "publishTimeDt", "qualifications", "refreshLevelType", "roles", "rootOrgId", "salary", "salary60", "salaryReal", "score", "scoreState", "skillLabel", "staff", "staffId", "tags", "tradingArea", "welfareLabel", "workCity", "workingExp", "workingExpCode"]
+describe('【职位领域】推荐职位 - requestRecommendJobs', function () {
   it('1. 空参数', () => {
     return Job.requestRecommendJobs().then(() => {
       assert(false)
@@ -32,13 +34,15 @@ describe('职位领域: requestRecommendJobs', function () {
       expect(err.message).to.be.equal('简历编号不能为空')
     })
   })
-  it('2. at、rt不正确', () => {
+  it('2. at、rt必需且正确', () => {
     return Job.requestRecommendJobs(Object.assign({}, baseData, {
       resumeNumber: DEFAULT_CONFIG.resumeNumber,
       at: 'DEFAULT_CONFIG.at',
       rt: 'DEFAULT_CONFIG.rt'
     })).then(res => {
-      expect(res.statusDescription).to.contain('用户凭证失效')
+      expect(res.statusDescription).to.contain('用户凭证失效') // .and.expect(res.statusCode).to.be.equal(206)
+    }).catch(err => {
+      assert(false)
     })
   })
   it('3. 正常参数', () => {
@@ -47,8 +51,225 @@ describe('职位领域: requestRecommendJobs', function () {
       at: DEFAULT_CONFIG.at,
       rt: DEFAULT_CONFIG.rt
     })).then(res => {
-      expect(res.data.list).to.be.an('array').and.to.be.not.empty.and.to.have.all.keys(recommendJobKeys)
-      // expect(res.data.list).to.include.keys()
+      expect(res.data.list[0]).to.have.all.keys(recommendJobKeys)
+    }).catch(err => {
+      assert(false)
+    })
+  })
+})
+
+/**
+ * 搜索职位
+ */
+
+
+/**
+ * 获取职位详情
+ * 不需要at/rt
+ */
+const jobDetailKeys = ["companyDetail", "positionDetail", "positionFeedback", "positionGrayDetail"]
+describe('【职位领域】获取职位详情 - requestJobDetail', function () {
+  it('1. 空参数', () => {
+    return Job.requestJobDetail().then(() => {
+      assert(false)
+    }).catch(err => {
+      expect(err.message).to.be.equal('职位编号不能为空')
+    })
+  })
+  it('2. 正常参数', () => {
+    return Job.requestJobDetail(Object.assign({}, baseData, {
+      number: DEFAULT_CONFIG.jobNumber,
+      at: DEFAULT_CONFIG.at,
+      rt: DEFAULT_CONFIG.rt
+    })).then(res => {
+      expect(res.data).to.have.all.keys(jobDetailKeys)
+    }).catch(err => {
+      assert(false)
+    })
+  })
+})
+
+/**
+ * 获取相似职位
+ * 不需要at/rt
+ */
+const similarJobsKeys = ["chatTotal", "chatWindowState", "count", "informationStream", "jobLabel", "list", "method", "typeSearch"]
+describe('【职位领域】获取相似职位 - requestSimilarJobs', function () {
+  it('1. 空参数', () => {
+    return Job.requestSimilarJobs().then(() => {
+      assert(false)
+    }).catch(err => {
+      expect(err.message).to.be.equal('职位编号不能为空')
+    })
+  })
+  it('2. 正常参数', () => {
+    return Job.requestSimilarJobs(Object.assign({}, baseData, {
+      number: DEFAULT_CONFIG.jobNumber,
+      at: DEFAULT_CONFIG.at,
+      rt: DEFAULT_CONFIG.rt
+    })).then(res => {
+      expect(res.data).to.have.all.keys(similarJobsKeys)
+    }).catch(err => {
+      assert(false)
+    })
+  })
+})
+
+/**
+ * 获取在招职位
+ * 不需要at/rt
+ */
+const areaJobsKeys = ["chatTotal", "chatWindowState", "count", "informationStream", "jobLabel", "list", "method", "typeSearch"]
+describe('【职位领域】获取在招职位 - requestAreaJobs', function () {
+  it('1. 空参数', () => {
+    return Job.requestAreaJobs().then(() => {
+      assert(false)
+    }).catch(err => {
+      expect(err.message).to.be.equal('公司ID不能为空')
+    })
+  })
+  it('2. 正常参数', () => {
+    return Job.requestAreaJobs(Object.assign({}, baseData, {
+      companyNumber: DEFAULT_CONFIG.companyNumber,
+      cityId: '530',
+      at: DEFAULT_CONFIG.at,
+      rt: DEFAULT_CONFIG.rt
+    })).then(res => {
+      expect(res.data).to.have.all.keys(areaJobsKeys)
+    }).catch(err => {
+      assert(false)
+    })
+  })
+})
+
+/**
+ * 获取在招职位所在城市
+ */
+const areaJobCityKeys = ["city", "jobType"]
+describe('【职位领域】获取在招职位所在城市 - requestRecommendJobs', function () {
+  it('1. 空参数', () => {
+    return Job.requestAreaJobCity().then(() => {
+      assert(false)
+    }).catch(err => {
+      expect(err.message).to.be.equal('职位编号不能为空')
+    })
+  })
+  it('2. 正常参数', () => {
+    return Job.requestAreaJobCity(Object.assign({}, baseData, {
+      number: DEFAULT_CONFIG.jobNumber,
+      at: DEFAULT_CONFIG.at,
+      rt: DEFAULT_CONFIG.rt
+    })).then(res => {
+      expect(res.data).to.have.all.keys(areaJobCityKeys)
+    }).catch(err => {
+      assert(false)
+    })
+  })
+})
+
+/**
+ * 收藏、取消收藏职位
+ */
+describe('【职位领域】收藏、取消收藏职位 - requestJobCollection', function () {
+  it('1. 空参数', () => {
+    return Job.requestJobCollection().then(() => {
+      assert(false)
+    }).catch(err => {
+      expect(err.message).to.be.equal('职位编号不能为空')
+    })
+  })
+  it('2. at、rt必需且正确', () => {
+    return Job.requestJobCollection(Object.assign({}, baseData, {
+      number: DEFAULT_CONFIG.jobNumber,
+      at: 'DEFAULT_CONFIG.at',
+      rt: 'DEFAULT_CONFIG.rt'
+    })).then(res => {
+      expect(res.statusDescription).to.contain('用户凭证失效')
+    }).catch(err => {
+      assert(false)
+    })
+  })
+  it('3. 正常参数', () => {
+    return Job.requestJobCollection(Object.assign({}, baseData, {
+      number: DEFAULT_CONFIG.jobNumber,
+      at: DEFAULT_CONFIG.at,
+      rt: DEFAULT_CONFIG.rt
+    })).then(res => {
+      expect(res.statusCode).to.be.equal(200)
+    }).catch(err => {
+      assert(false)
+    })
+  })
+  it('4. 收藏职位', () => {
+    return Job.requestJobCollection(Object.assign({}, baseData, {
+      number: DEFAULT_CONFIG.jobNumber,
+      at: DEFAULT_CONFIG.at,
+      rt: DEFAULT_CONFIG.rt,
+      operateType: 3
+    })).then(res => {
+      expect(res.statusCode).to.be.equal(200)
+    }).catch(err => {
+      assert(false)
+    })
+  })
+  it('5. 取消收藏职位', () => {
+    return Job.requestJobCollection(Object.assign({}, baseData, {
+      number: DEFAULT_CONFIG.jobNumber,
+      at: DEFAULT_CONFIG.at,
+      rt: DEFAULT_CONFIG.rt,
+      operateType: 7
+    })).then(res => {
+      expect(res.statusCode).to.be.equal(200)
+    }).catch(err => {
+      assert(false)
+    })
+  })
+})
+
+/**
+ * 投递职位
+ */
+const jobDeliverKeys = ["actionCode", "actionValue", "failDelivery", "repeatDelivery", "successDelivery"]
+describe('【职位领域】投递职位 - requestJobDeliver', function () {
+  it('1. 空参数', () => {
+    return Job.requestJobDeliver().then(() => {
+      assert(false)
+    }).catch(err => {
+      expect(err.message).to.be.equal('职位编号不能为空')
+    })
+  })
+  it('2. at、rt必需且正确', () => {
+    return Job.requestJobDeliver(Object.assign({}, baseData, {
+      number: DEFAULT_CONFIG.jobNumber,
+      resumeNumber: DEFAULT_CONFIG.resumeNumber,
+      deliveryWay: 0,
+      stPage: 4020,
+      askPageCode: '',
+      cycleType: 0,
+      cityId: '530',
+      at: 'DEFAULT_CONFIG.at',
+      rt: 'DEFAULT_CONFIG.rt'
+    })).then(res => {
+      expect(res.statusDescription).to.contain('用户凭证失效')
+    }).catch(err => {
+      assert(false)
+    })
+  })
+  it('3. 正常参数', () => {
+    return Job.requestJobDeliver(Object.assign({}, baseData, {
+      number: DEFAULT_CONFIG.jobNumber,
+      resumeNumber: DEFAULT_CONFIG.resumeNumber,
+      deliveryWay: 0,
+      stPage: 4020,
+      askPageCode: '',
+      cycleType: 0,
+      cityId: '530',
+      at: DEFAULT_CONFIG.at,
+      rt: DEFAULT_CONFIG.rt
+    })).then(res => {
+      expect(res.data).to.have.all.keys(jobDeliverKeys)
+    }).catch(err => {
+      assert(false)
     })
   })
 })
